8b96d64613a66c87639fceed633ed27f2410294b
João Lopes <joaomarcoslopes@id.uff.br>
2020-10-04 19:18:25 -0300

    test: improved cases

--- packet.test.py
+++ packet.test.py
@@ -2,8 +2,11 @@ from packet import generate_parity_bit
 
 
 def test_generate_parity_bit():
-    assert generate_parity_bit([1, 1, 0, 0]) == 0, "Should be 0"
+    assert generate_parity_bit([0, 0, 0, 0]) == 0, "Should be 0"
     assert generate_parity_bit([1, 0, 0, 0]) == 1, "Should be 1"
+    assert generate_parity_bit([1, 1, 0, 0]) == 0, "Should be 0"
+    assert generate_parity_bit([1, 1, 1, 0]) == 1, "Should be 1"
+    assert generate_parity_bit([1, 1, 1, 1]) == 0, "Should be 0"
 
 
 if __name__ == '__main__':
