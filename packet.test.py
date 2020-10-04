bf7e8e7558568d0619a3d1fd9fa71d5df5bdcef8
João Lopes <joaomarcoslopes@id.uff.br>
2020-10-04 19:42:45 -0300

    feat: transpose_colums

--- packet.test.py
+++ packet.test.py
@@ -1,4 +1,4 @@
-from packet import generate_parity_bit
+from packet import generate_parity_bit, transpose_columns
 
 
 def test_generate_parity_bit():
@@ -9,6 +9,15 @@ def test_generate_parity_bit():
     assert generate_parity_bit([1, 1, 1, 1]) == 0, "Should be 0"
 
 
+def test_transpose_columns():
+    assert transpose_columns([[0, 1, 1, 0], [1, 1, 1, 0], [1, 1, 1, 1], [1, 0, 0, 0]]) == [
+        [0, 1, 1, 1], [1, 1, 1, 0], [1, 1, 1, 0], [0, 0, 1, 0]], "Should be transposed"
+
+    assert transpose_columns([[1, 0, 0, 0], [1, 1, 0, 0], [1, 1, 1, 0], [1, 1, 1, 1]]) == [
+        [1, 1, 1, 1], [0, 1, 1, 1], [0, 0, 1, 1], [0, 0, 0, 1]], "Should be transposed"
+
+
 if __name__ == '__main__':
     test_generate_parity_bit()
+    test_transpose_columns()
     print("Testes passaram!")
